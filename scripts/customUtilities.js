149589128b2324050d2e891e7edb3cdc8b9eae46
mdaley15 <mdaley@inter-state.com>
2025-03-28 12:47:13 -0700

    Commit

--- scripts/customUtilities.js
+++ scripts/customUtilities.js
@@ -65,7 +65,7 @@ $( document ).ready(function() {
 
     var slides = document.querySelectorAll('#home .slide > img');
     slides.forEach((slide, index) => {
-        var wrap = document.createElement('div');
+        var wrap = document.createElement('div').classList.add('slideImgWrap');
         slide.parentNode.insertBefore(wrap, slide);
         wrap.appendChild(slide);
     });
