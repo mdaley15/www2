a9e239e8d73f55b39757aad52b4f00732a037a62
mdaley15 <mdaley@inter-state.com>
2025-03-31 15:04:18 -0700

    Commit

--- scripts/changeSign.js
+++ scripts/changeSign.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
         });
     
         if($(this).hasClass("active")) {
+            console.log('Active');
             $(this).children(".sign").html('<i class="fa-solid fa-minus"></i>');
         } else {
             $(this).children(".sign").html('<i class="fa-solid fa-plus"></i>');
