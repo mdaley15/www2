1ce73db1b8a7e8017319d93f38754973d2b0a98a
mdaley15 <mdaley@inter-state.com>
2025-03-31 15:37:49 -0700

    Commit

--- scripts/customUtilities.js
+++ scripts/customUtilities.js
@@ -103,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let watermark = document.querySelector('.watermark');
     let pageIcon = document.querySelector('.introTitle i');
-    console.log(pageIcon.classList)
+    console.log(pageIcon.classList[1])
 });
 function imgFilter(btn) {
     let filter = btn.getAttribute('data-filter');
