601738f0cb38ac51d005fc14c1b90d2034c623b6
mdaley15 <mdaley@inter-state.com>
2025-04-04 11:48:02 -0700

    Commit

--- scripts/customUtilities.js
+++ scripts/customUtilities.js
@@ -44,6 +44,8 @@ $( document ).ready(function() {
         // console.log(ariaLabel);
     });
 
+    var headerLinkRemoveAttr = $('.whatWeDo > .Header-nav-item:nth-child(2) > .Header-nav-folder-title');
+    console.log(headerLinkRemoveAttr);
     $('.whatWeDo > .Header-nav-item:nth-child(2) > .Header-nav-folder-title').removeAttr('href');
 
     $(".sqs-search-page").closest("main.Main").prepend('<section class = "Intro search-page-intro"> </section>');
