79e518807f98c22a78ce2dd4cc4286bd74679ab8
mdaley15 <mdaley@inter-state.com>
2025-04-16 11:57:56 -0700

    Commit

--- scripts/changeSign.js
+++ scripts/changeSign.js
@@ -3,13 +3,12 @@ $(document).ready(function() {
     $(".collapsible-header").click(function () {
         var current = this;
         let otherHeaders = $(this).closest('li').siblings('li').find('.collapsible-header');
-        console.log(otherHeaders);
             otherHeaders.children(".fa-solid").removeClass('fa-minus').addClass('fa-plus');
-        $(".collapsible-header").each(function() {
-            // if($(this) !== current) {
-            //     $(this).children(".fa-solid").removeClass('fa-minus').addClass('fa-plus');
-            // }
-        });
+        // $(".collapsible-header").each(function() {
+        //     if($(this) !== current) {
+        //         $(this).children(".fa-solid").removeClass('fa-minus').addClass('fa-plus');
+        //     }
+        // });
     
         if($(this).hasClass("active")) {
             $(this).children(".fa-solid").removeClass('fa-minus').addClass('fa-plus');
