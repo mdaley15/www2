2bd1c029fc94035690db95d76980e4f452079226
mdaley15 <mdaley@inter-state.com>
2025-04-19 13:10:42 -0700

    Commit

--- scripts/scrollStacking.js
+++ scripts/scrollStacking.js
@@ -7,8 +7,11 @@ document.addEventListener('DOMContentLoaded', function() {
         const chunkSize = 2;
         
         for (let i = 0; i < elements.length; i += chunkSize) {
-            const card = document.createElement('div'); // Create wrapper div
-            card.classList.add('card'); // Add class to the wrapper (optional)
+            const cardWrapper = document.createElement('div'); // Create wrapper div
+            cardWrapper.classList.add('cardWrapper'); // Add class to the wrapper
+            const card = document.createElement('div'); // Create card div
+            card.classList.add('card'); // Add class to the card
+            cardWrapper.appendChild(card);
             
             // Append elements to the wrapper
             for (let j = 0; j < chunkSize && i + j < elements.length; j++) {
@@ -24,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
             //         gallery.appendChild(card);
             //     }
             // }
-            gallery.appendChild(card);
+            gallery.appendChild(cardWrapper);
         }
     }
 });
