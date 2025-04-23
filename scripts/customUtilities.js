82096eba01f115c478c9acdc74dba021b70c33b7
mdaley15 <mdaley@inter-state.com>
2025-04-23 09:47:16 -0700

    Commit

--- scripts/customUtilities.js
+++ scripts/customUtilities.js
@@ -11,7 +11,6 @@ $( document ).ready(function() {
     const allLinks = document.querySelectorAll('a');
     allLinks.forEach(link => {
         if (link.textContent.includes('inter-state')) {
-            console.log(link);
             link.classList.add('noWrap');
         }
     });
