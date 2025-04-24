f66777103e111c0f01363ee8a2f7fce38f6b40c8
mdaley15 <mdaley@inter-state.com>
2025-04-24 11:16:23 -0700

    Commit

--- scripts/customUtilities.js
+++ scripts/customUtilities.js
@@ -5,9 +5,27 @@ function preventAnchorScroll() {
     };
     if (window.location.hash) {
         // handler is executed at most once
-        $(window).one('scroll', scrollToTop);
+        var hash = window.location.hash.substring(1);
+        const target = document.getElementById(hash);
+        console.log(target);
+        $('body').removeClass('is-mobile-overlay-active');
+        var offset = -100; // <-- change the value here
+        if (target) {
+            let expanded = target.getAttribute('aria-expanded');
+            if (expanded) {
+                console.log(expanded);
+                // target.click();
+            }
+            console.log(target.getBoundingClientRect().top);
+            console.log(target.getBoundingClientRect().top + offset);
+            setTimeout(function() {
+                $('html, body').animate({
+                    scrollTop: target.getBoundingClientRect().top + offset
+                }, 1000);
+            }, 500);
+            return false;
+        }
     }
-
     // make sure to release scroll 1 second after document readiness
     // to avoid negative UX
     $(function () {
