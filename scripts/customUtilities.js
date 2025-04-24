b74bde8a897fa889d365eae7262fcefba3e49372
mdaley15 <mdaley@inter-state.com>
2025-04-24 15:23:21 -0700

    Commit

--- scripts/customUtilities.js
+++ scripts/customUtilities.js
@@ -17,7 +17,7 @@ $( document ).ready(function() {
 
     $('.whatWeDo .Header-nav-item:first-child > a.Header-nav-folder-title').removeAttr('href');
     $('.whatWeDo .Header-nav-item:nth-child(2) > .Header-nav-folder-title').attr('href', '/yearbooks');
-    $('.Mobile-overlay-nav--secondary .Mobile-overlay-nav-item:nth-child(2) > span').contents().unwrap().wrap('<a href="/yearbooks"></a>');
+    $('.Mobile-overlay-nav--secondary .Mobile-overlay-nav-item:nth-child(2) > span').contents().unwrap().wrap('<a></a>');
 
     var pathName = window.location.pathname;
     if (pathName.includes("/yearbooks")) {
