7b36e5ec72ecf6f74ba3c635bd1bc05fbef84071
mdaley15 <mdaley@inter-state.com>
2025-05-02 11:16:27 -0700

    Commit

--- scripts/backToTop.js
+++ scripts/backToTop.js
@@ -3,7 +3,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     var scrollHeight = 76;
-    if (window.scrollY > 800) {
+    if (window.scrollY > 500) {
         toTopBtn.style.display = "block";
     } else {
         toTopBtn.style.display = "none";
