f7dba8dd866d0b36d02d6020c9da51c2e74e8c90
mdaley15 <mdaley@inter-state.com>
2025-05-02 12:37:50 -0700

    Commit

--- scripts/backToTop.js
+++ scripts/backToTop.js
@@ -3,19 +3,16 @@ let mainHeader = document.querySelector('.Header--bottom');
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    console.log(window.scrollY);
     if (window.scrollY > 500) {
-        toTopBtn.style.display = "block";
+        toTopBtn.style.display = "flex";
     } else {
         toTopBtn.style.display = "none";
     }
 }
 function scrollToTop() {
-    // console.log(window.scrollY);
     window.scrollTo({
         top: 0,
         left: 0,
         behavior: "smooth"
     });
-    // mainHeader.scrollIntoView({ block: "start", behavior: "smooth" })
 }
