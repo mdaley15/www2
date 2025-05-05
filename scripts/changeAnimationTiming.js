138cedfc7224ebaffb7ed8fcb1314972a1d2a315
mdaley15 <mdaley@inter-state.com>
2025-05-05 11:18:44 -0700

    Commit

--- scripts/changeAnimationTiming.js
+++ scripts/changeAnimationTiming.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let stats = document.querySelectorAll('.stats .contentOuter');
     console.log(stats);
     stats.forEach(stat => {
-        console.log(window.width);
-        if (window.width < 1300) {
-            console.log(window.width);
+        console.log(window.innerWidth);
+        if (window.innerWidth < 1300) {
+            console.log(window.innerWidth);
             stat.setAttribute('data-aos-anchor-placement', 'top-bottom');
             stat.setAttribute('data-aos-delay', '200');
         }
