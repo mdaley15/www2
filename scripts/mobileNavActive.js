743b389a9a4210252a332a58769df5bc6f7a3e2c
mdaley15 <mdaley@inter-state.com>
2025-05-06 11:31:33 -0700

    Commit

--- scripts/mobileNavActive.js
+++ scripts/mobileNavActive.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var pathName = window.location.pathname;
     console.log(pathName);
+    let mobileNav = document.querySelectorAll('.Mobile-overlay-menu-main a');
+    console.log(mobileNav);
 });
