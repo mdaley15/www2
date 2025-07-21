dec10a74cb9b9dcef76e1c401a0b7a5d6b97d4d4
mdaley15 <mdaley@inter-state.com>
2025-07-21 12:12:13 -0700

    Commit

--- scripts/customUtilities.js
+++ scripts/customUtilities.js
@@ -367,6 +367,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function lightboxFilter(itemLink) {
     var pathName = window.location.pathname;
     console.log(pathName);
+    console.log(itemLink);
     const filterClasses = [];
     let thisBlock = itemLink.closest('.collapsible-body');
     let thisGallery = thisBlock.querySelector('.sqs-gallery-design-grid');
